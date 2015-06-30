a5d1b84a730bd87531d6f1c377c1ab55bfdf00a6
Nathan Day <nathan.day98@icloud.com>
2015-06-30 14:37:16 +0100

    Added crappy GenUIID

--- banana/editor.cpp
+++ banana/editor.cpp
@@ -3,6 +3,15 @@
 #include "rendering.h"
 #include "ui.h"
 
+global_variable int ui_count = 0;
+
+internal UIID GenUIID()
+{
+	ui_count += 1;
+	UIID result = { 0, 0, ui_count};
+	return result;
+}
+
 void EditorUpdateAndRender(EditorMemory *editor_memory, InputData *input,
 	RenderContext *render_context, UIContext *ui_context, float delta)
 {
@@ -17,11 +26,11 @@ void EditorUpdateAndRender(EditorMemory *editor_memory, InputData *input,
 
 	BeginRenderer(render_context);
 
-	if (GUIButton(ui_context, { 0, 0, 1 }, "WHITE", 400.0f, 400.0f, 300.0f, 100.0f))
+	if (GUIButton(ui_context, GenUIID(), "WHITE", 400.0f, 400.0f, 300.0f, 100.0f))
 	{
 	}
 
-	if (GUIButton(ui_context, { 0, 0, 2 }, "RED", 800.0f, 800.0f, 300.0f, 100.0f))
+	if (GUIButton(ui_context, GenUIID(), "RED", 800.0f, 800.0f, 300.0f, 100.0f))
 	{
 	}
 
